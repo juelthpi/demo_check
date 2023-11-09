98223e1ccfdab387a2bcbb33567628c8dc7215e5
rana <juel@gmail.com>
2023-11-09 19:27:29 +0600

    demo

---
+++ assets/js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	// search
+	$('.menu_right_area ul li i').click(function(){
+		$('.mobile_search_box').slideToggle();
+		return false
+	});
+	$('.menu_right_mobile_show ul li i').click(function(){
+		$('.mobile_search_box').slideToggle();
+		return false
+	});
+	//Banner Carousel
+	$('.student_review_inner_area').owlCarousel({
+		items:1,
+		loop:true,
+		nav:true,
+		dots:true,
+		autoplay:false,
+		navText: ['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+	});
+	 // fanci box
+	 $(".version_video_icon a").fancybox({
+	});
+	
+});
+// loader
+$(window).on("load", function () {
+	$("#preloader").fadeOut();
+	$("#preloader").delay(500).fadeOut("slow");
+	$("body").delay(500).css({ overflow: "visible" });
+  });
+$('.counter').counterUp({
+	delay: 10,
+	time: 1000
+});
+$('.couter_about').counterUp({
+	delay: 10,
+	time: 2000
+});
+
+// scrollup
+$(window).on('scroll', function() { if ($(this).scrollTop() > 500) { $('.scrollup').fadeIn(); } else { $('.scrollup').fadeOut(); } }); $('.scrollup').on("click", function() { $("html, body").animate({ scrollTop: 0 }, 800); return false; }); 
+function myFunction(x) {
+	x.classList.toggle("change");
+  }
+
+  
